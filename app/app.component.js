c5b90aa6efbcd95304c23b851e6b003ddf8bcf60
Brandon Perkins <perkins.brandon@gmail.com>
2017-06-25 10:56:29 -0400

    added databound radio buttons for image orientation

--- app/app.component.js
+++ app/app.component.js
@@ -5,25 +5,87 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'POSTERIZER';
+        this.entries = [];
+        this.selectedEntry = {
+            value: null,
+            description: null
+        };
+        this.orientation = "";
     }
+    AppComponent.prototype.ngOnInit = function () {
+        this.entries = [
+            {
+                description: 'Portrait ',
+                value: 1
+            },
+            {
+                description: 'Landscape ',
+                value: 2
+            }
+        ];
+        //default to first
+        if (this.entries) {
+            this.onOrientationChange(this.entries[0]);
+        }
+    };
     AppComponent.prototype.handleImage = function (e) {
         var imageLoader = document.getElementById('imageLoader');
         var canvas = document.getElementById('imageCanvas');
         var ctx = canvas.getContext('2d');
         var reader = new FileReader();
+        if (this.orientation == 'Portrait') {
+            console.log(this.orientation);
+            ctx.canvas.width = '400%';
+            ctx.canvas.height = '600%';
+        }
+        else {
+            console.log(this.orientation);
+            ctx.canvas.width = '700%';
+            ctx.canvas.height = '400%';
+        }
         reader.onload = function (event) {
             var img = new Image();
             img.onload = function () {
-                ctx.drawImage(img, 0, 0);
+                //calculate image size and position
+                var wrh = img.width / img.height;
+                var newWidth = canvas.width;
+                var newHeight = newWidth / wrh;
+                if (newHeight > canvas.height) {
+                    newHeight = canvas.height;
+                    newWidth = newHeight * wrh;
+                }
+                var xOffset = newWidth < canvas.width ? ((canvas.width - newWidth) / 2) : 0;
+                var yOffset = newHeight < canvas.height ? ((canvas.height - newHeight) / 2) : 0;
+                //clear canvas of previous images to prevent overlap
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                //draw image - adjust to fit canvas and preserve aspect ratio
+                ctx.drawImage(img, xOffset, yOffset, newWidth, newHeight);
             };
             img.src = event.target.result;
         };
         reader.readAsDataURL(e.target.files[0]);
+    };
+    // handleOrientation() {
+    //     let portrait = document.getElementById('orientationRB_1').getAttribute('checked');
+    //     if (portrait == 'true') {
+    //         alert('portrait');
+    //     }
+    //     else {
+    //         alert('landscape');
+    //     }
+    //     return portrait;
+    // }
+    AppComponent.prototype.onOrientationChange = function (entry) {
+        this.selectedEntry = Object.assign({}, this.selectedEntry, entry);
+        this.orientation = entry.description;
     };
     return AppComponent;
 }());
@@ -32,7 +94,8 @@ AppComponent = __decorate([
         selector: 'pc-app',
         templateUrl: './app/app.component.html',
         styleUrls: ['./app/app.component.css']
-    })
+    }),
+    __metadata("design:paramtypes", [])
 ], AppComponent);
 exports.AppComponent = AppComponent;
 //# sourceMappingURL=app.component.js.map
--- app/app.component.ts
+++ app/app.component.ts
@@ -6,20 +6,88 @@ import { Component } from '@angular/core';
     styleUrls: ['./app/app.component.css']
 })
 export class AppComponent {
-    title:string = 'POSTERIZER';
+    title: string = 'POSTERIZER';
+    entries: Array<any> = [];
+    selectedEntry: { [key: string]: any } = {
+        value: null,
+        description: null
+    };
+    orientation: string = "";
     
+    constructor() {
+    }
+
+    ngOnInit() {
+        this.entries = [
+            {
+                description: 'Portrait ',
+                value: 1
+            },
+            {
+                description: 'Landscape ',
+                value: 2
+            }
+        ];
+
+        //default to first
+        if (this.entries) {
+            this.onOrientationChange(this.entries[0]);
+        }
+    }
+
     handleImage(e: any) {
         let imageLoader = document.getElementById('imageLoader');
         let canvas: any = document.getElementById('imageCanvas');
         let ctx = canvas.getContext('2d');
-        var reader = new FileReader();
+        let reader = new FileReader();
+
+        if (this.orientation == 'Portrait') {
+            console.log(this.orientation);
+            ctx.canvas.width = '400%';
+            ctx.canvas.height = '600%';
+        }
+        else {
+            console.log(this.orientation);
+            ctx.canvas.width = '700%';
+            ctx.canvas.height = '400%';
+        }
+
         reader.onload = function (event: any) {
             var img = new Image();
             img.onload = function () {
-                ctx.drawImage(img, 0, 0);
+                //calculate image size and position
+                var wrh = img.width / img.height;
+                var newWidth = canvas.width;
+                var newHeight = newWidth / wrh;
+                if (newHeight > canvas.height) {
+                    newHeight = canvas.height;
+                    newWidth = newHeight * wrh;
+                }
+                var xOffset = newWidth < canvas.width ? ((canvas.width - newWidth) / 2) : 0;
+                var yOffset = newHeight < canvas.height ? ((canvas.height - newHeight) / 2) : 0;
+                //clear canvas of previous images to prevent overlap
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                //draw image - adjust to fit canvas and preserve aspect ratio
+                ctx.drawImage(img, xOffset, yOffset, newWidth, newHeight);
             }
             img.src = event.target.result;
         }
         reader.readAsDataURL(e.target.files[0]);
+    }
+
+    // handleOrientation() {
+    //     let portrait = document.getElementById('orientationRB_1').getAttribute('checked');
+    //     if (portrait == 'true') {
+    //         alert('portrait');
+    //     }
+    //     else {
+    //         alert('landscape');
+    //     }
+    //     return portrait;
+    // }
+
+    onOrientationChange(entry:any) {
+        this.selectedEntry = Object.assign({}, this.selectedEntry, entry);
+        this.orientation = entry.description;
     }
 }
