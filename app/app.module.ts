b610fdb72f6ec1c7618d39e6cae8077b20eb97e7
Brandon Perkins <perkins.brandon@gmail.com>
2017-06-26 14:12:05 -0400

    css change

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { enableProdMode } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ColorPickerComponent } from './colorPicker/colorPicker.component';
